e4b8ddd741cd8297706a260966555bb5755c0153
pepesan <pepesan@gmailcom>
2019-03-26 18:01:28 +0100

    metida la integración con swagger ui

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var librosRouter = require('./routes/libros');
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./openapi/swagger.json');
 
 var app = express();
 
@@ -22,7 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/libros', librosRouter);
+app.use('/api/libros', librosRouter);
+// Ruta de Swagger UI
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
